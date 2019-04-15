90323f3b5435931e6814ac9acc956a8763d87538
dimasarz <32926808+dimasarz@users.noreply.github.com>
2019-04-15 14:10:42 +0700

    tambah api key

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=zy4fPvwkCtb8WhkfKA8btWqFk6px7sdi9i0er27s";
         
         $("#loadImage").click(function(){
             $.ajax({
